5310deee0f5f339a2d23e2224fadc7ea80b9a82d
Anthony-0908 <anthonyyuvega09@gmail.com>
2023-09-21 09:49:10 +0800

    done with the login and register

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref , onMounted} from 'vue';
+
+import axios from 'axios';
+const user = ref();
+onMounted(async () => {
+    const data = await axios.get('/api/user');
+    console.log(data);
+})
+</script>
+
+<template>
+    <div class="max-w-7xl mx-auto">
+      <div v-if="authStore.user">
+        <h1>{{ authStore.user.name }}</h1>
+        <p>{{ authStore.user.email }}</p>
+      </div>
+      <div v-else>
+        <h1>Go and login</h1>
+      </div>
+    </div>
+  </template>
